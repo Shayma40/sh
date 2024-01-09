e3415dee005b75e1f24cad118dbe7d7dd20b1cf5
Shayma40 <150466639+Shayma40@users.noreply.github.com>
2024-01-09 16:16:53 +0100

    add index.html new file

---
+++ main.js
@@ -0,0 +1,114 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Gateau chocolat noisette',
+        image: 'G1.png',
+        price: 65000
+    },
+    {
+        id: 2,
+        name: 'Gateau noisette pistache',
+        image: 'G3.png',
+        price: 90000
+    },
+    {
+        id: 3,
+        name: 'Gateau chocolat amande',
+        image: 'G5.png',
+        price: 55000
+    },
+    {
+        id: 4,
+        name: 'Entremet chocolat ',
+        image: 'G10.png',
+        price: 75000
+    },
+    {
+        id: 5,
+        name: 'Opera',
+        image: 'G6.png',
+        price: 100000
+    },
+    {
+        id: 6,
+        name: 'Gateau mousse au chocolat noisette ',
+        image: 'G7.png',
+        price: 60000
+    },
+    {
+        id: 7,
+        name: 'Entremet chocolat blanc ',
+        image: 'G9.png',
+        price: 80000
+    },
+]
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
